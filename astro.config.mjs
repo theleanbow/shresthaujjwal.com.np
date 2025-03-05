082dc77e39210e160e057d81a7201e85d09f3f34
LeanBow <theleanbow@gmail.com>
2025-03-05 16:57:40 +0545

    feat: Integrate Tailwind CSS and shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://shresthaujjwal.com.np',
   integrations: [
     sitemap({
-      // Set to true to exclude pages with draft: true from the sitemap
       filter: (page) => page.search(/\/draft-/) === -1,
-      // Add a custom changefreq (default is 'weekly')
       changefreq: 'weekly',
-      // Add a custom priority (default is 0.7)
       priority: 0.7,
-      // Add a custom lastmod (defaults to the current date)
       lastmod: new Date(),
-      // Define a custom serializer function
       serialize(item) {
-        // Add custom logic here for more control
         return item;
       }
     }),
   ],
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    }
+  }
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx,astro}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
