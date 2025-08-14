ea8652eefe05fd462bf5898b6b1542a908267caa
LeanBow <theleanbow@gmail.com>
2025-08-14 22:39:06 +0545

    chore: update to latest astro template version
    Reveret the shadcn imports as well

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
-import sitemap from '@astrojs/sitemap';
 
-export default defineConfig({
-  site: 'https://shresthaujjwal.com.np',
-  integrations: [
-    sitemap({
-      filter: (page) => page.search(/\/draft-/) === -1,
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-      serialize(item) {
-        return item;
-      }
-    }),
-  ],
-  vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        '@': '/src'
-      }
-    }
-  }
-});
+// https://astro.build/config
+export default defineConfig({});
